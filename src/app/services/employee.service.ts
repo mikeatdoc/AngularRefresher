042468fd4d9794e34d0072c2557ef7fd8131680f
mikeatdoc <37195590+mikeatdoc@users.noreply.github.com>
2018-03-16 10:41:14 -0700

    Added working service call to Employee model

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Employee } from '../models/employee';
+import { Employee } from '../shared/employee';
 
 @Injectable()
 export class EmployeeService {
 
   constructor() { }
 
-GetEmployee() : number {
+GetEmployee2() : number {
   return 343;
 }
-//  GetEmployee : Employee {
-//    Employee e = new Employee();
+ GetEmployee() : Employee {
+    let e = new Employee();
 
-//    e.FirstName = "bob";
+   e.FirstName = "bob";
+   e.LastName = "barker";
 
-//    return e;
-//    }
+   return e;
+   }
 
 }
--- src/app/timecard/timecard.component.ts
+++ src/app/timecard/timecard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 
 import { EmployeeService } from "../services/employee.service";
 
-
+import { Employee } from '../shared/employee';
 
 
 @Component({
@@ -18,7 +18,8 @@ export class TimeCardComponent implements OnInit {
     this._employeeService= employeeService;
   }
 
-  ee = '44';
+  //ee = '44';
+  ee: Employee;
 
   ngOnInit() {
     this.ee = this._employeeService.GetEmployee();
